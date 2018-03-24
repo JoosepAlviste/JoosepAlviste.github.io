85607cb8f5cb9c520f2ed5ada674b2b9f4c1e769
Joosep Alviste <joosep.alviste@gmail.com>
2018-03-24 22:37:00 +0100

    Rename app.scss to main.scss

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import '../styles/app.scss'
+import '../styles/main.scss'
 
 const Index = ({ children }) => <div>{children()}</div>
 
