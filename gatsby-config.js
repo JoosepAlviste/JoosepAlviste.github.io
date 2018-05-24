cd9a68b994efd09b1d5c0a520431e3064373d204
Joosep Alviste <joosep.alviste@gmail.com>
2018-05-24 19:44:36 +0200

    Move Google Analytics to head section

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-113623528-1',
-        head: false,
+        head: true,
         anonymize: true,
         respectDNT: true,
       },
